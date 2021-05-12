7235451ece6f125f1b5a68b8f8f44bfe679b1a18
Carlods97 <84104581+Carlods97@users.noreply.github.com>
2021-05-12 16:32:18 -0500

    Ejemplo 001 NodeJS

---
+++ nodejs-ejemplo01/index.js
@@ -0,0 +1,13 @@
+const math = require('./math.js');
+
+
+console.log(math);
+
+
+/*
+console.log(math.add(1, 9));
+console.log(math.substract(1, 9));
+console.log(math.multiply(1, 9));
+console.log(math.divide(1, 0));
+
+*/
